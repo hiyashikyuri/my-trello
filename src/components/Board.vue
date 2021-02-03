66c0e4f00ec2511cc3319be8c01063419ef9de28
Masahiro Okubo <stmhamachiii@gmail.com>
2021-02-04 00:02:33 +0900

    listを表示する修正

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,18 +5,32 @@
     </header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <list v-for="(item, index) in lists"
+            :key="item.id"
+            :title="item.title"
+            :listIndex="index"
+      />
       <list-add />
     </main>
   </div>
 </template>
 
 <script>
-  import ListAdd from "./ListAdd";
+  import List from './List'
+  import ListAdd from "./ListAdd"
+  import { mapState } from 'vuex'
+
   export default {
     components: {
+      List,
       ListAdd
-    }
+    },
 
+    computed: {
+      ...mapState([
+        'lists'
+      ])
+    }
   }
 
 </script>
